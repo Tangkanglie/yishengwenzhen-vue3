b1a94bcff338110a8940374aece987a20aae9b44
Tangkanglie <402379191@qq.com>
2024-05-23 23:14:49 +0800

    feat: pinia代码抽离

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import pinia from '../src/stores/index'
 
 import App from './App.vue'
 import router from './router'
@@ -8,7 +7,7 @@ import 'vant/lib/index.css'
 import './styles/style.css'
 
 const app = createApp(App)
-app.use(createPinia().use(piniaPluginPersistedstate))
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
